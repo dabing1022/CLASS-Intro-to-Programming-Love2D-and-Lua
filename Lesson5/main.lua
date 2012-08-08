8d77a346392abf0558276798553b754fbadf7130
Rachel J. Morris <Rachel@Moosader.com>
2012-08-08 06:36:29 -0500

    Added code example for part 5 of the guide.
    http://www.moosader.com/resources/tutorials/love-lua/5

---
+++ Lesson5/main.lua
@@ -0,0 +1,21 @@
+function love.load()
+    playerX = 0
+    playerSpeed = 1
+	screenWidth = 800
+end
+
+function love.update()
+    -- Move the player
+    playerX = playerX + playerSpeed
+	
+	-- Change direction if player goes off-screen
+	if ( playerX > screenWidth ) then 	-- Off the right side
+		playerSpeed = -1
+	elseif ( playerX < 0 ) then 		-- Off the left side
+		playerSpeed = 1
+	end
+end
+
+function love.draw()
+    love.graphics.print( "(O_O)", playerX, 20 )
+end 
