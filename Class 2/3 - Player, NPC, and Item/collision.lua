1d4a321f62c7966721f9561adee1df04f2f074c0
Rachel J. Morris <Rachel@Moosader.com>
2012-08-11 11:00:07 -0500

    made collision function more liberal

--- Class 2/3 - Player, NPC, and Item/collision.lua
+++ Class 2/3 - Player, NPC, and Item/collision.lua
@@ -6,7 +6,7 @@
 function collision( x1, y1, x2, y2 )
     distance = ( (x2 - x1)^2 + (y2 - y1)^2 )^(0.5)
     
-    if ( distance < 16 ) then
+    if ( distance < 32 ) then
         return true
     else
         return false
